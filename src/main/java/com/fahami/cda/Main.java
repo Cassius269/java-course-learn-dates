ba0e5f3b4461c9570c4d05f8ccf9b4a502d02689
Cassius269 <fahamygaston@gmail.com>
2025-11-09 17:23:41 +0100

    gérer des dates dans un code historique

--- src/main/java/com/fahami/cda/Main.java
+++ src/main/java/com/fahami/cda/Main.java
@@ -1,7 +1,19 @@
 package com.fahami.cda;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Date date = new Date(1762704343003l);
+        IO.println(date);
+
+        Calendar currentDate = Calendar.getInstance();
+        IO.println(currentDate.getTime());
+        currentDate.set(currentDate.YEAR,  2022);// changer l'année
+        currentDate.set(currentDate.HOUR_OF_DAY, 00); // changer l'heure de la journée
+        IO.println(currentDate.getTime());
     }
 }
